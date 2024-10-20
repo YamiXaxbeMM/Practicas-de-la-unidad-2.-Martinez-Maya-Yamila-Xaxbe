90b054914acad741460a5699b53a3fbd2d6b3c3b
YamiXaxbeMM <l23580470@reynosa.tecnm.mx>
2024-10-19 23:45:20 -0500

    Add files via upload

---
+++ P1_C++.cpp
@@ -0,0 +1,28 @@
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+int main(){
+	
+	cout <<"Hola, este es un programa que cuenta los numero ala inversa" << "\n";
+	
+	int A,B,C,D;
+	
+	cout << "Por favor ingrese el primer valor A: " "\n";
+	cin >> A;
+	
+	cout << "Por favor ingrese el primer valor B: " << "\n";
+	cin >> B;
+	
+	cout << "Por favor ingrese el primer valor C: " << "\n";
+	cin >> C;
+	
+	cout << "Por favor ingrese el primer valor D: " << "\n";
+	cin >> D;
+	
+	cout << "Los numeros ingresados son: " << "\n";
+	cout << D << "," << C << "," << B << "," << A ;
+	
+	return 0;
+}
